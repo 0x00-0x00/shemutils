1330beaa647790639e11312634c586550c1394f9
shemhazai <andre.marques@fatec.sp.gov.br>
2016-11-21 10:53:11 -0200

    Bootstrap shell script to install dependencies

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Written by shemhazai 0x00-0x00
+# Downloads all dependencies
+
+echo "Read the README.md in the repository to find more information about module usage!"
+sleep 1
+echo "3 ..."
+sleep 1
+echo "2 ..."
+sleep 1
+echo "1 ..."
+sleep 1
+
+echo "[*] Trying to download dependencies..."
+pip install rsa pycrypto gevent
+
+echo "Bootstrap has terminated."
+
