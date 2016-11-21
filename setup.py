a37e96b93310d5589fb6d1f4f64fefe143bc10e2
shemhazai <andre.marques@fatec.sp.gov.br>
2016-11-21 12:14:08 -0200

    Added url to github repository in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['shemutils'],
     package_dir={"shemutils": "src"},
     package_data={"shemutils": ["src/*"]},
-    url='http://github.com/0x00-0x00',
+    url='http://github.com/0x00-0x00/shemutils',
     license='MIT',
     author='shemhazai',
     author_email='andre.marques@fatec.sp.gov.br',
