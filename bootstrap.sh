57399c60af77fb962ca679c554e7484a12430e88
shemhazai <andre.marques@fatec.sp.gov.br>
2016-12-04 10:00:49 -0200

    Added admin check to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,13 @@
 # Written by shemhazai 0x00-0x00
 # Downloads all dependencies
 
+uid=$(id -u)
+if [ "$uid" != "0" ]; then
+    echo "You need administrative privileges to install python modules."
+    exit
+fi
+
+
 echo "Read the README.md in the repository to find more information about module usage!"
 sleep 1
 echo "3 ..."
